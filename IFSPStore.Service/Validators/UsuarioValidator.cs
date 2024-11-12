321654bd3314f9f8505bcb0dc0354b0aeed31f17
Kauê Alcântara Leivas <kaueleivas0@gmail.com>
2024-11-12 08:46:19 -0300

    alteração da senha

--- IFSPStore.Service/Validators/UsuarioValidator.cs
+++ IFSPStore.Service/Validators/UsuarioValidator.cs
@@ -15,14 +15,19 @@ namespace IFSPStore.Service.Validators
 
             // Valida o campo Login
             RuleFor(u => u.Login)
-                .NotEmpty().WithMessage("O login do usuário é obrigatório.")
+                .MaximumLength(25).WithMessage("A senha não pode ter mais de 25 caracteres")
+                .Matches(@"[A-Z]+").WithMessage("A senha deve conter uma letra maiúscula")
+                .Matches(@"[a-z]+").WithMessage("A senha deve conter uma letra minúscula")
+                .Matches(@"[0-9]+").WithMessage("A senha deve conter um número")
+                .Matches(@"[\!\@\#\$\%\¨\'\&\*\(\)\-\_\+\=\/\\\?\°\~\|\^\]\[\}\{\º\ª]+").WithMessage("A senha deve conter um caracter especial")
                 .MaximumLength(45).WithMessage("O login do usuário não pode exceder 45 caracteres.");
 
             // Valida o campo Senha
             RuleFor(u => u.Senha)
                 .NotEmpty().WithMessage("A senha do usuário é obrigatória.")
                 .MinimumLength(8).WithMessage("A senha do usuário deve ter pelo menos 8 caracteres.")
-                .MaximumLength(45).WithMessage("A senha do usuário não pode exceder 45 caracteres.");
+                .MaximumLength(45).WithMessage("A senha do usuário não pode exceder 45 caracteres.")
+                ;
 
             // Valida o campo DataCadastro
             RuleFor(u => u.DataCadastro)
